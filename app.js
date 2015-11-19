f633a69f20e2f736e857b9873f7c70050e3d053d
Ben Spoon <bens@aweber.com>
2015-11-19 15:28:10 -0500

    Dynamic routes based on api key

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var jsonServer = require('json-server');
+var jsonServer = require('json-server'),
+    fs = require('fs');
 
 // Returns an Express server
 var server = jsonServer.create();
@@ -6,26 +7,24 @@ server.set('port', process.env.PORT || 3000);
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(jsonServer.defaults());
 
+fs.readdir('./data/', function(err, files) {
+  if (err) throw err;
+  files.forEach(function(file) {
+    server.use('/'+file.replace('.json', ''), jsonServer.router('data/'+file));
+  })
+});
 
 // Add custom routes
 server.get('/getApiKey', function (req, res) {
   var key = require('crypto').randomBytes(4).toString('hex');
-
   // Create the json file with the key name
   var jsonfile = require('jsonfile'),
       stubs = jsonfile.readFileSync('data/data.json');
-
   jsonfile.writeFile('data/'+key+'.json', stubs, function(err) {
-    console.log("created file " + key);
+    server.use('/'+key, jsonServer.router('data/'+key+'.json'));
   });
-
  // Send the key back as the response
  res.json({ apiKey: key });
-
 });
-
-// Returns an Express router
-var router = jsonServer.router('data/data.json');
-server.use(router);
 
 server.listen(server.get('port'));
